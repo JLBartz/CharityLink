18edd4b6a84e8c6a8a8c0876acf100898a739841
JLBartz <barte.jameslorenz@gmail.com>
2025-06-27 15:11:50 +0800

    New .py and .ui files

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from PyQt6.uic import loadUi
 from PyQt6 import QtWidgets
-from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtWidgets import QDialog, QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QDialog
 import sys
 
-class loginScreen(QDialog):
+class loginWindow(QDialog):
     def __init__(self):
-        super(loginScreen, self).__init__()
+        super(loginWindow, self).__init__()
         loadUi("loginWindow.ui", self)
 
 app = QApplication(sys.argv)
-welcome = loginScreen()
+log = loginWindow()
 widget = QtWidgets.QStackedWidget()
-widget.addWidget(welcome)
-widget.setFixedHeight(800)
-widget.setFixedHeight(1200)
+widget.addWidget(log)
+widget.setWindowTitle("CharityLink")
+widget.setFixedHeight(600)
+widget.setFixedWidth(1000)
 widget.show()
 try:
     sys.exit(app.exec())
 except:
-    print("Exiting the program")
+    print("Exiting the program...")
